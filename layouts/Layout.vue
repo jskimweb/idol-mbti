4402de860a1ba52722fae2615d34db3b724c8422
jskimweb <jskimweb@gmail.com>
2023-05-10 01:16:06 +0900

    모바일 브라우저 주소창에 대한 컨텐츠 영역 높이 처리

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -11,11 +11,19 @@
 <script setup lang="ts">
 import MainHeader from './MainHeader.vue';
 import MainFooter from './MainFooter.vue';
+
+if (process.client) setScreenSize();
+if (process.client) window.addEventListener('resize', setScreenSize);
+
+function setScreenSize() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
 </script>
 
 <style lang="scss" scoped>
 .content {
-  height: $main-content-height;
+  height: calc(var(--vh, 1vh) * 100 - #{$main-header-height} - #{$main-footer-height});
   background: linear-gradient(to top, white, rgb(70, 203, 255));
   padding: 16px;
   overflow-y: auto;
