36726f391bd9eea6663e723fad24f7913b5a77dd
jskimweb <jskimweb@gmail.com>
2023-05-11 02:08:24 +0900

    로딩/빈 목록 어포던스 추가

--- pages/list.vue
+++ pages/list.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="list-page">
-    <div v-for="item in list" :key="item.id" class="card">
-      <span class="pixel top-left" />
-      <span class="pixel top-right" />
-      <span class="pixel bot-left" />
-      <span class="pixel bot-right" />
-      <div class="card-text-group">
-        <span class="card-text">{{ item.name }}</span>
-        <span class="card-text">{{ item.group }}</span>
+    <span v-if="isLoading" class="loading">로딩중...</span>
+    <span v-else-if="isEmpty" class="empty">빈 목록입니다.</span>
+    <div v-else class="card-list">
+      <div v-for="item in list" :key="item.id" class="card">
+        <span class="pixel top-left" />
+        <span class="pixel top-right" />
+        <span class="pixel bot-left" />
+        <span class="pixel bot-right" />
+        <div class="card-text-group">
+          <span class="card-text">{{ item.name }}</span>
+          <span class="card-text">{{ item.group }}</span>
+        </div>
+        <img class="card-image" :src="item.photo" alt="프로필 사진" />
       </div>
-      <img class="card-image" :src="item.photo" alt="프로필 사진" />
     </div>
   </div>
 </template>
@@ -20,7 +24,10 @@ const supabase = inject('supabase');
 const route = useRoute();
 const router = useRouter();
 
+const isLoading = ref(false);
 const list = ref([]);
+
+const isEmpty = computed(() => list.value.length === 0);
 
 fetchList();
 
@@ -28,21 +35,26 @@ async function fetchList() {
   const { mbti, gender } = route.query;
   if (!mbti) return router.replace({ name: 'index' });
 
-  if (gender === 'all') {
-    const { data } = await supabase
-      .from('idol-mbti')
-      .select()
-      .eq('mbti', mbti)
-      .order('name', { ascending: true });
-    list.value = data;
-  } else {
-    const { data } = await supabase
-      .from('idol-mbti')
-      .select()
-      .eq('mbti', mbti)
-      .eq('gender', gender)
-      .order('name', { ascending: true });
-    list.value = data;
+  isLoading.value = true;
+  try {
+    if (gender === 'all') {
+      const { data } = await supabase
+        .from('idol-mbti')
+        .select()
+        .eq('mbti', mbti)
+        .order('name', { ascending: true });
+      list.value = data;
+    } else {
+      const { data } = await supabase
+        .from('idol-mbti')
+        .select()
+        .eq('mbti', mbti)
+        .eq('gender', gender)
+        .order('name', { ascending: true });
+      list.value = data;
+    }
+  } finally {
+    isLoading.value = false;
   }
 }
 </script>
@@ -52,59 +64,72 @@ $pixel: 8px;
 
 .list-page {
   position: relative;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(calc(#{$pixel} * 30), 1fr));
-  gap: 10px;
-  .pixel {
+  height: 100%;
+  .loading,
+  .empty {
     position: absolute;
-    width: $pixel;
-    height: $pixel;
-    background-color: black;
-    &.top-left {
-      top: calc(0% + #{$pixel});
-      left: calc(0% + #{$pixel});
-    }
-    &.top-right {
-      top: calc(0% + #{$pixel});
-      right: calc(0% + #{$pixel});
-    }
-    &.bot-left {
-      bottom: calc(0% + #{$pixel});
-      left: calc(0% + #{$pixel});
-    }
-    &.bot-right {
-      bottom: calc(0% + #{$pixel});
-      right: calc(0% + #{$pixel});
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .card-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(calc(#{$pixel} * 30), 1fr));
+    gap: 10px;
+    padding-bottom: 16px;
+  }
+}
+
+.list-page .card-list .card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: calc(#{$pixel} * 30);
+  height: calc(#{$pixel} * 40);
+  background: url('~/assets/img/card.png') no-repeat center;
+  background-size: contain;
+  padding: $pixel;
+  margin: 0 auto;
+  .card-text-group {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .card-text {
+      display: block;
+      font-size: 20px;
+      text-align: center;
+      color: white;
     }
   }
-  .card {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: calc(#{$pixel} * 30);
-    height: calc(#{$pixel} * 40);
-    background: url('~/assets/img/card.png') no-repeat center;
-    background-size: contain;
-    padding: $pixel;
-    margin: 0 auto;
-    .card-text-group {
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%, -50%);
-      .card-text {
-        display: block;
-        font-size: 20px;
-        text-align: center;
-        color: white;
-      }
-    }
-    .card-image {
-      height: 100%;
-      object-fit: cover;
-    }
+  .card-image {
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+.list-page .card-list .card .pixel {
+  position: absolute;
+  width: $pixel;
+  height: $pixel;
+  background-color: black;
+  &.top-left {
+    top: calc(0% + #{$pixel});
+    left: calc(0% + #{$pixel});
+  }
+  &.top-right {
+    top: calc(0% + #{$pixel});
+    right: calc(0% + #{$pixel});
+  }
+  &.bot-left {
+    bottom: calc(0% + #{$pixel});
+    left: calc(0% + #{$pixel});
+  }
+  &.bot-right {
+    bottom: calc(0% + #{$pixel});
+    right: calc(0% + #{$pixel});
   }
 }
 </style>
